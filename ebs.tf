e87d6a53a546af6af703bd58e1f009cfa60314cb
Ryan Faircloth <ryan@dss-i.com>
2022-10-07 12:35:44 -0400

    feat: initial checkin

---
+++ ebs.tf
@@ -0,0 +1,39 @@
+module "irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "${var.uniqueName}_${var.release}_${var.chart}"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.eks_oidc_provider_arn
+      namespace_service_accounts = ["${var.namespace}:${var.sa}"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+
+
+resource "helm_release" "ebs_csi" {
+
+  name             = var.release
+  namespace        = var.namespace
+  repository       = var.repository
+  chart            = var.chart
+  version          = var.version
+  create_namespace = false
+
+  values = var.values
+
+  set {
+    name  = var.value_arn
+    value = module.ebs_cni_irsa.iam_role_arn
+    type  = "string"
+  }
+}
+
