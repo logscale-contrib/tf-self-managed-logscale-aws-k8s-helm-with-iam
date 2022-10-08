af54ae627329ee87b0d5b7870501053c591d53a6
Ryan Faircloth <ryan@dss-i.com>
2022-10-08 11:28:06 -0400

    fix: typos

--- app.tf
+++ app.tf
@@ -5,8 +5,8 @@ resource "kubectl_manifest" "app" {
       repository    = var.repository
       chart_version = var.chart_version
       release       = var.release
-      value_arn     = value.value_arn
+      value_arn     = var.value_arn
       iam_role_arn  = module.irsa.iam_role_arn
       values        = var.values
-  namespace = value.namespace })
+  namespace = var.namespace })
 }
