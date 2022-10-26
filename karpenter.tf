240f58fc94c44b90338168e7f73a9aba758b4557
Ryan Faircloth <ryan@dss-i.com>
2022-10-26 12:47:42 -0400

    fix: provisioners

--- karpenter.tf
+++ karpenter.tf
@@ -6,9 +6,32 @@ resource "aws_iam_instance_profile" "karpenter" {
 
 resource "kubectl_manifest" "karpenter_provisioners" {
   depends_on = [
-    module.release
+    module.release,
+    kubectl_manifest.AWSNodeTemplate
   ]
-  #count     = var.attach_karpenter_controller_policy ? 1 : 0
   for_each  = var.karpenter_provisioners
   yaml_body = each.value
+}
+
+resource "kubectl_manifest" "AWSNodeTemplate" {
+  count = var.attach_karpenter_controller_policy ? 1 : 0
+  
+  depends_on = [
+    module.release
+  ]
+  yaml_body = <<YAML
+apiVersion: karpenter.k8s.aws/v1alpha1
+kind: AWSNodeTemplate
+metadata:
+  name: default
+spec:
+  subnetSelector:                             # required
+    kubernetes.io/cluster/${var.uniqueName}: "shared"
+  securityGroupSelector:                      # required, when not using launchTemplate
+    karpenter.sh/discovery/${var.uniqueName}: ${var.uniqueName}
+  instanceProfile: ${aws_iam_instance_profile.karpenter[0].name}          # optional, if already set in controller args
+  # launchTemplate: MyLaunchTemplate            # optional, see Launch Template documentation
+  # tags:
+  #   InternalAccountingTag: "1234"             # optional, add tags for your own use
+YAML
 }
